30ca76cb154f46fa17192e7504505441dab00e7f
wacky <kevin.young8931@gmail.com>
2021-03-06 01:21:46 +0100

    replaced jsx with

--- wacket.js
+++ wacket.js
@@ -1,3 +1,7 @@
-const element = <h1 title="foo">Hello</h1>
+const element = React.createElement(
+    "h1",
+    {title:"foo"},
+    "Hello"
+)
 const container = document.getElementById("root");
 ReactDOM.render(element,container);
