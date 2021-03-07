64805d693a695cb194ab92990eaabf2e47faccb1
wacky <kevin.young8931@gmail.com>
2021-03-07 21:31:20 +0100

    using our own JSX element for babel to transpile

--- src/Step_1.js
+++ src/Step_1.js
@@ -26,11 +26,18 @@ const Wacket = {
     createElement
 }
 
-const element = Wacket.createElement(
-    "div",
-    {id: "foo"},
-    Wacket.createElement("a", null, "bar"),
-    Wacket.createElement("b")
+// const element = Wacket.createElement(
+//     "div",
+//     {id: "foo"},
+//     Wacket.createElement("a", null, "bar"),
+//     Wacket.createElement("b")
+// )
+/** @jsx Didact.createElement */
+const element = (
+    <div id="foo">
+        <a>bar</a>
+        <b />
+    </div>
 )
 const container = document.getElementById("root")
 ReactDOM.render(element, container)
